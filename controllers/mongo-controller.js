fdacc332532731f6d67706bb136a3b600da978d0
Joseph <jtawil2015@gmail.com>
2020-07-25 10:23:37 -0800

    added more comments

--- controllers/mongo-controller.js
+++ controllers/mongo-controller.js
@@ -27,21 +27,23 @@ module.exports = {
     addNote : (req,res)=> {
         db.notes.insert({text: req.body.text, completed: req.body.completed, tags: []},
             (err,data) => err ? res.send(err) : res.send(data));
-    }, 
+    },
 
+    //amyTime we are referencing an id you must include mongojs.ObjectID()
     updateText : (req,res) => {
         db.notes.update({_id: mongojs.ObjectID( req.body.id)},{$set: {text: req.body.text}},(err,data) => {
             err ? send(err) : res.send(data);
         });
     },
 
+    //adding a single object to an array, we using the push method
     addTag: (req, res) => {
         db.notes.update({_id: mongojs.ObjectID(req.body.id)},{$push:{tags: req.body.tag}},(err,data)=>
         {
             err ? res.send(err) : res.send(data);
         });
     },
-
+    //adding multiple objects to an array, we also use the push method and the each method for each tag
     addTags: (req, res) => {
         db.notes.update({_id: mongojs.ObjectID(req.body.id)},{$push:{tags: {$each: req.body.tags}}},(err,data)=>
         {
@@ -49,6 +51,7 @@ module.exports = {
         });
     },
 
+    //to pull a multiple item from an array just use $pullAll
     removeTags: (req,res) => {
         db.notes.update({_id: mongojs.ObjectID(req.body.id)},
         { $pullAll: {tags: req.body.tags}},(err,data)=>
@@ -56,6 +59,8 @@ module.exports = {
             err ? res.send(err) : res.send(data);
         });
     },
+
+     //to pull a single item from an array just use $pull
     removeTag: (req,res) => {
         db.notes.update({_id: mongojs.ObjectID(req.body.id)},
         { $pull: {tags: req.body.tag}},(err,data)=>
@@ -63,7 +68,8 @@ module.exports = {
             err ? res.send(err) : res.send(data);
         });
     },
-    
+    // remove an object from the collection by its id,
+    // any parameter such as name can be used
     deleteNote: (req,res) => {
         db.notes.remove({_id: mongojs.ObjectID(req.body.id)},(err,data)=>
         {
